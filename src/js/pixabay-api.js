31e79aa648a470fc5de9fedcd813f35c7e4c8568
dvankevich <dvankevich@gmail.com>
2024-08-07 21:32:05 +0300

    add search form styles

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,10 @@
 import response from '../testdata/cats.json';
 
+/**
+ *
+ * @param {string} searchTerm
+ * @returns JSON object
+ */
 export function getImages(searchTerm) {
   return response;
 }
