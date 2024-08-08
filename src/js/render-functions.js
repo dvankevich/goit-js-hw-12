2ecb14c1e8e43f3d4fbb75037f94d308999bca4b
dvankevich <dvankevich@gmail.com>
2024-08-08 21:10:23 +0300

    remove clearGallery function

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,7 @@
 const myGallery = document.querySelector('.gallery');
 
 const itemMarkdown = `    <li class="gallery-item">
-      <a href="" class="galery-item-image-link">
+      <a href="https://pixabay.com/get/gf64bcba6d887d2c3b6bc019500819bd1cbb1d6722145784ce67b367a2a4bb6592136f3862fa13eb0c54d115ffde01998603e8bd3cdaae73cc9c5ce79485e7e38_1280.jpg" class="galery-item-image-link">
         <img class="galery-item-image"
           src="https://pixabay.com/get/g6c45b412d164ddb540292fe4a772cf6f1eb0f664be5f90ab44685310b9bd4d962e65ab1264913e40ec529661deac2d02941cf845ab60cede687f3d24d09f9b15_640.jpg"
           alt="simba, cat, portrait" />
@@ -27,6 +27,14 @@ const itemMarkdown = `    <li class="gallery-item">
       </ul>
     </li>`;
 
+// webformatURL — посилання на маленьке зображення для списку карток у галереї
+// largeImageURL — посилання на велике зображення для модального вікна
+// tags — рядок з описом зображення. Підійде для атрибута alt
+// likes — кількість вподобайок
+// views — кількість переглядів
+// comments — кількість коментарів
+// downloads — кількість завантажень
+
 let galleryMarkdown = '';
 for (let index = 0; index < 6; index++) {
   galleryMarkdown += itemMarkdown;
@@ -36,10 +44,6 @@ export function drawGallery(gallery, markdown) {
   gallery.innerHTML = markdown;
 }
 
-export function clearGallery(gallery) {
-  gallery.innerHTML = '';
-}
-
-clearGallery(myGallery);
+drawGallery(myGallery, '');
 
 drawGallery(myGallery, galleryMarkdown);
