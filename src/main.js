20ba7856f0439c152740c8ed948e4bacd03ed2e6
dvankevich <dvankevich@gmail.com>
2024-08-09 19:22:35 +0300

    add button event listener. add user input check

--- src/main.js
+++ src/main.js
@@ -4,12 +4,31 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const loadMessageMarkdown =
+  '<li class="load-message">Loading images, please wait...</li>';
 const myGallery = document.querySelector('.gallery');
+const searchButton = document.querySelector('.search-button');
+const searchInput = document.querySelector('.search-input');
 
-//drawGallery(myGallery, '');
+searchButton.addEventListener('click', function (event) {
+  let searchTerm = String(searchInput.value.trim());
+  searchTerm = searchTerm.replace(/[*]/g, ''); // видалення спецсимволів
+  event.preventDefault();
+  console.log(searchTerm.length);
+
+  if (!searchTerm || searchTerm.length < 3) {
+    console.log('Enter data for search, please. Min. 3 symbols.');
+    return;
+  }
+
+  console.log(`fetch data from backend with search term: ${searchTerm}`);
+  drawGallery(myGallery, loadMessageMarkdown);
+});
 
 let galleryMarkdown = '';
 let images = getImages().hits;
+
+//drawGallery(myGallery, loadMessageMarkdown);
 
 galleryMarkdown = getGalleryMarkdown(images);
 //drawGallery(myGallery, galleryMarkdown);
